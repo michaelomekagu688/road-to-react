b959eee5b08a95fcfd4df7c31e65c54797a1bd75
Omekagu Michael <michaelomekagu@gmail.com>
2025-07-16 13:34:42 +0100

    Worked on simple props, arrays and objects. Understood very well how to use '.map' method. (page 6 - 30

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { isLabelWithInternallyDisabledControl } from '@testing-library/user-event/dist/utils';
+
+//const title = 'Bitch'
+
+// function getTitle(title){
+//   return title
+// }
+
+// const numberss = [1,2,3,4,5];
+
+//  const newNumbers = numberss.map(function(number){
+//   return (
+//     number
+//   )
+//  })
+
+const details = [
+  {
+    id: 0,
+    name: "michael omekagu",
+    age:34,
+    course:" Electrical Engineering"
+  },
+  {
+    id: 1,
+    name: "Titi Chukwu",
+    age:12,
+    course:" Nursing"
+  },
+  {
+    id: 2,
+    name: "Kiki Lade",
+    age:21,
+    course:" Medical Labourtory Science"
+  },
+  {
+    id: 3,
+    name: "Putu Chukwu",
+    age:30,
+    course:"Pilot"
+  },
+]
+
+const detailDiv = details.map(function(soloDetail){
+  return(
+    <div key={soloDetail.id}> 
+      <div> Hey i am {soloDetail.name}, and i am {soloDetail.age} years old. </div>
+    </div>
+  )
+})
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> {detailDiv} </div>
+      <div> Hello world </div>
+        
+      {/* how to comment */}
     </div>
   );
 }
