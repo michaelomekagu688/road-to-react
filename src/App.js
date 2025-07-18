c47139a7f8ce6cbdac60a35f8ada4b230b8bba8d
Omekagu Michael <michaelomekagu@gmail.com>
2025-07-18 19:37:20 +0100

    learnt states, callbacks and handlers in jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import { isLabelWithInternallyDisabledControl } from '@testing-library/user-event/dist/utils';
+
+import React from "react";
 
 //const title = 'Bitch'
 
@@ -16,52 +15,88 @@ import { isLabelWithInternallyDisabledControl } from '@testing-library/user-even
 //   )
 //  })
 
-const details = [
-  {
-    id: 0,
-    name: "michael omekagu",
-    age:34,
-    course:" Electrical Engineering"
-  },
-  {
-    id: 1,
-    name: "Titi Chukwu",
-    age:12,
-    course:" Nursing"
-  },
-  {
-    id: 2,
-    name: "Kiki Lade",
-    age:21,
-    course:" Medical Labourtory Science"
-  },
-  {
-    id: 3,
-    name: "Putu Chukwu",
-    age:30,
-    course:"Pilot"
-  },
-]
+// const details = [
+//   {
+//     id: 0,
+//     name: "michael omekagu",
+//     age:34,
+//     course:" Electrical Engineering"
+//   },
+//   {
+//     id: 1,
+//     name: "Titi Chukwu",
+//     age:12,
+//     course:" Nursing"
+//   },
+//   {
+//     id: 2,
+//     name: "Kiki Lade",
+//     age:21,
+//     course:" Medical Labourtory Science"
+//   },
+//   {
+//     id: 3,
+//     name: "Putu Chukwu",
+//     age:30,
+//     course:"Pilot"
+//   },
+// ]
 
-const detailDiv = details.map(function(soloDetail){
-  return(
-    <div key={soloDetail.id}> 
-      <div> Hey i am {soloDetail.name}, and i am {soloDetail.age} years old. </div>
-    </div>
-  )
-})
+// const detailDiv = details.map(function(soloDetail){
+//   return(
+//     <div key={soloDetail.id}> 
+//       <div> Hey i am {soloDetail.name}, and i am {soloDetail.age} years old. </div>
+//     </div>
+//   )
+// })
 
+
+const stories = [
+  {
+    title: "React",
+    url: 'https://reactjs.org/',
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redus",
+    url: 'https://redus.js.org/',
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
 
-  return (
-    <div className="App">
-      <div> {detailDiv} </div>
-      <div> Hello world </div>
-        
-      {/* how to comment */}
+  const [firstValue, secondValue] = React.useState('');
+
+  const handle = (event) => {
+     secondValue(event.target.value);
+  }
+
+
+  return(
+    <div>
+      <h1> Grubbly </h1>
+      <Search vals={firstValue} onSec={handle}/>
     </div>
-  );
+  )
+}
+
+const Search = (props) => {
+
+  return(
+    <div>
+      <label htmlFor="search"> Search </label>
+      <input type="text" id="serch" onChange={props.onSec} />
+      <div> Searching for {props.vals} </div>
+    </div>
+  )
+  
 }
 
 export default App;
